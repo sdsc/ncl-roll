65f3b0d576c44f23453db831f938a83e189b3284
Jerry Greenberg <jpg@gordon-ln1.sdsc.edu>
2018-10-16 18:52:52 -0700

    APS-883 ncl 6.5.1

--- src/ncl/version.mk
+++ src/ncl/version.mk
@@ -9,25 +9,26 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-ncl
-VERSION        = 6.1.2
-RELEASE        = 5
+VERSION        = 6.5.1
+RELEASE        = 0
 PKGROOT        = /opt/ncl
 
 SRC_SUBDIR     = ncl
 
-SOURCE_NAME    = ncl_ncarg
-SOURCE_SUFFIX  = tar.gz
+SOURCE_NAME    = ncl
+SOURCE_SUFFIX  = zip
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 SZIP_NAME      = szip
 SZIP_SUFFIX    = tar.gz
-SZIP_VERSION   = 2.1
+SZIP_VERSION   = 2.1.1
 SZIP_PKG       = $(SZIP_NAME)-$(SZIP_VERSION).$(SZIP_SUFFIX)
 SZIP_DIR       = $(SZIP_PKG:%.$(SZIP_SUFFIX)=%)
 
-TAR_GZ_PKGS    = $(SOURCE_PKG) $(SZIP_PKG)
+ZIP_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS    = $(SZIP_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
 RPM.PREFIX     = $(PKGROOT)
