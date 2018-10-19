0de1c31d317068055492a1f79ef0f93e3366652f
Jerry Greenberg <jpg@gordon-ln2.sdsc.edu>
2018-10-18 18:37:03 -0700

    APS-883 fix version number

--- src/ncl/version.mk
+++ src/ncl/version.mk
@@ -9,7 +9,7 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-ncl
-VERSION        = 6.5.1
+VERSION        = 6.5.0
 RELEASE        = 0
 PKGROOT        = /opt/ncl
 
@@ -27,7 +27,13 @@ SZIP_VERSION   = 2.1.1
 SZIP_PKG       = $(SZIP_NAME)-$(SZIP_VERSION).$(SZIP_SUFFIX)
 SZIP_DIR       = $(SZIP_PKG:%.$(SZIP_SUFFIX)=%)
 
-ZIP_PKGS       = $(SOURCE_PKG)
+TRIANGLE_NAME      = triangle
+TRIANGLE_SUFFIX    = zip
+TRIANGLE_VERSION   = 1.6
+TRIANGLE_PKG       = $(TRIANGLE_NAME)-$(TRIANGLE_VERSION).$(TRIANGLE_SUFFIX)
+TRIANGLE_DIR       = $(TRIANGLE_PKG:%.$(TRIANGLE_SUFFIX)=%)
+
+ZIP_PKGS       = $(SOURCE_PKG) $(TRIANGLE_PKG)
 TAR_GZ_PKGS    = $(SZIP_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
